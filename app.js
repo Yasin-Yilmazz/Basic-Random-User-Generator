fe6ea99b3fc8528911268d69e66ccbbe0bee2b69
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-15 13:23:30 +0300

    randım user generator

---
+++ app.js
@@ -0,0 +1,48 @@
+const button = document.querySelector("#btn");
+const innerContainer = document.querySelector(".inner-container");
+
+button.addEventListener("click", (e) => {
+  const getPersonData = async () => {
+    try {
+      const url = "https://randomuser.me/api";
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+      createPersonData(data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  const createPersonData = (data) => {
+    let { first, last } = data.results[0].name;
+    let location = data.results[0].location.country;
+    let { email, phone } = data.results[0];
+    let image = data.results[0].picture.medium;
+    console.log(first, last, location, email, phone);
+
+    innerContainer.innerHTML = `
+    <img class="image" src="${image}" alt="" />
+        <p>
+          <i class="fa-solid fa-user"></i> Name :
+          <span class="name"> ${first}</span>
+        </p>
+        <p>
+          <i class="fa-solid fa-user"></i> LastName :
+          <span class="last-name"> ${last} </span>
+        </p>
+        <p>
+          <i class="fa-solid fa-magnifying-glass-location"></i> Location :
+          <span class="location"> ${location}</span>
+        </p>
+        <p>
+          <i class="fa-solid fa-phone-flip"></i>Phone :
+          <span class="phone"> ${phone}</span>
+        </p>
+        <p>
+          <i class="fa-solid fa-envelope"></i>Email :
+          <span class="email"> ${email}</span>
+        </p>
+    `;
+  };
+  getPersonData();
+});
